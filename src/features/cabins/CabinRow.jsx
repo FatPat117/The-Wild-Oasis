e3b2fcf558921b6c7cc597079e52358c9faf9796
Phat117 <ntphat110705@gmail.com>
2025-04-04 23:36:01 +0700

    Building a Reusable Table

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -6,19 +6,20 @@ import { formatCurrency } from "../../utils/helpers";
 import CreateCabinForm from "./CreateCabinForm";
 import { useCreateCabin } from "./useCreateCabin";
 import { useDeleteCabin } from "./useDeleteCabin";
+import Table from "../../ui/Table";
 
-/* eslint-disable react/prop-types */
-const TableRow = styled.div`
-        display: grid;
-        grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-        column-gap: 2.4rem;
-        align-items: center;
-        padding: 1.4rem 2.4rem;
+// /* eslint-disable react/prop-types */
+// const TableRow = styled.div`
+//         display: grid;
+//         grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+//         column-gap: 2.4rem;
+//         align-items: center;
+//         padding: 1.4rem 2.4rem;
 
-        &:not(:last-child) {
-                border-bottom: 1px solid var(--color-grey-100);
-        }
-`;
+//         &:not(:last-child) {
+//                 border-bottom: 1px solid var(--color-grey-100);
+//         }
+// `;
 
 const Img = styled.img`
         display: block;
@@ -65,7 +66,7 @@ function CabinRow({ cabin }) {
 
         return (
                 <>
-                        <TableRow role="row">
+                        <Table.Row role="row">
                                 <Img src={image} />
                                 <Cabin>{name}</Cabin>
                                 <div>Fits up to {maxCapacity} guests</div>
@@ -86,12 +87,12 @@ function CabinRow({ cabin }) {
                                                         <CreateCabinForm cabinToEdit={cabin} />
                                                 </Modal.Window>
 
-                                                <Modal.Open>
+                                                <Modal.Open opens="delete">
                                                         <button disabled={isDeleting}>
                                                                 <HiTrash />
                                                         </button>
                                                 </Modal.Open>
-                                                <Modal.Window>
+                                                <Modal.Window name="delete">
                                                         <ConfirmDelete
                                                                 resourceName="cabins"
                                                                 disabled={isDeleting}
@@ -100,7 +101,7 @@ function CabinRow({ cabin }) {
                                                 </Modal.Window>
                                         </Modal>
                                 </div>
-                        </TableRow>
+                        </Table.Row>
                 </>
         );
 }
