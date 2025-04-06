e2e754a437eea7ab21ceac10f14db5ccef2c628d
Phat117 <ntphat110705@gmail.com>
2025-04-06 17:04:21 +0700

    Client-Side Filtering: Filtering Cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,9 +1,9 @@
-import styled from "styled-components";
+import { useSearchParams } from "react-router-dom";
+import Menus from "../../ui/Menus";
 import Spinner from "../../ui/Spinner";
 import Table from "../../ui/Table";
 import CabinRow from "./CabinRow";
 import { useFetchCabins } from "./useFetchCabins";
-import Menus from "../../ui/Menus";
 
 // const Table = styled.div`
 //         border: 1px solid var(--color-grey-200);
@@ -14,25 +14,35 @@ import Menus from "../../ui/Menus";
 //         overflow: hidden;
 // `;
 
-const TableHeader = styled.header`
-        display: grid;
-        grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-        column-gap: 2.4rem;
-        align-items: center;
+// const TableHeader = styled.header`
+//         display: grid;
+//         grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+//         column-gap: 2.4rem;
+//         align-items: center;
 
-        background-color: var(--color-grey-50);
-        border-bottom: 1px solid var(--color-grey-100);
-        text-transform: uppercase;
-        letter-spacing: 0.4px;
-        font-weight: 600;
-        color: var(--color-grey-600);
-        padding: 1.6rem 2.4rem;
-`;
+//         background-color: var(--color-grey-50);
+//         border-bottom: 1px solid var(--color-grey-100);
+//         text-transform: uppercase;
+//         letter-spacing: 0.4px;
+//         font-weight: 600;
+//         color: var(--color-grey-600);
+//         padding: 1.6rem 2.4rem;
+// `;
 
 function CabinTable() {
         const { isLoading, cabins, error } = useFetchCabins();
+        const [searchParams, setSearchParams] = useSearchParams();
         if (isLoading) return <Spinner />;
 
+        const filterValue = searchParams.get("discount") || "all";
+
+        let filteredCabins;
+
+        if (filterValue === "all") filteredCabins = cabins;
+
+        if (filterValue === "no-discount") filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+
+        if (filterValue === "with-discount") filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
         return (
                 <Menus>
                         <Table role="table" columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
@@ -46,7 +56,7 @@ function CabinTable() {
                                 </Table.Header>
 
                                 <Table.Body
-                                        data={cabins}
+                                        data={filteredCabins}
                                         render={(cabin) => <CabinRow cabin={cabin} key={cabin.id}></CabinRow>}
                                 />
                         </Table>
