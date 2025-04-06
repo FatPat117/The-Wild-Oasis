21d18dfb73f10cc9609da46a4b28d021780e570e
Phat117 <ntphat110705@gmail.com>
2025-04-06 16:14:06 +0700

    Building a Reusable Context Menu

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,12 +1,13 @@
 import { HiPencil, HiSquare2Stack, HiTrash } from "react-icons/hi2";
 import styled from "styled-components";
 import ConfirmDelete from "../../ui/ConfirmDelete";
+import Menus from "../../ui/Menus";
 import Modal from "../../ui/Modal";
+import Table from "../../ui/Table";
 import { formatCurrency } from "../../utils/helpers";
 import CreateCabinForm from "./CreateCabinForm";
 import { useCreateCabin } from "./useCreateCabin";
 import { useDeleteCabin } from "./useDeleteCabin";
-import Table from "../../ui/Table";
 
 // /* eslint-disable react/prop-types */
 // const TableRow = styled.div`
@@ -73,32 +74,41 @@ function CabinRow({ cabin }) {
                                 <Price>{formatCurrency(regularPrice)}</Price>
                                 {discount ? <Discount>{formatCurrency(discount)}</Discount> : <span>&mdash;</span>}
                                 <div>
-                                        <button onClick={() => handleDuplicate()}>
-                                                <HiSquare2Stack />
-                                        </button>
-
                                         <Modal>
-                                                <Modal.Open opens="edit">
-                                                        <button>
-                                                                <HiPencil />
-                                                        </button>
-                                                </Modal.Open>
-                                                <Modal.Window name="edit">
-                                                        <CreateCabinForm cabinToEdit={cabin} />
-                                                </Modal.Window>
+                                                <Menus.Menu>
+                                                        <Menus.Toggle id={cabinId} />
+                                                        <Menus.List id={cabinId}>
+                                                                <Menus.Button
+                                                                        icon={<HiSquare2Stack />}
+                                                                        onClick={() => handleDuplicate()}
+                                                                >
+                                                                        Duplicate
+                                                                </Menus.Button>
+                                                                <Modal.Open opens="edit">
+                                                                        <Menus.Button icon={<HiPencil />}>
+                                                                                Edit
+                                                                        </Menus.Button>
+                                                                </Modal.Open>
 
-                                                <Modal.Open opens="delete">
-                                                        <button disabled={isDeleting}>
-                                                                <HiTrash />
-                                                        </button>
-                                                </Modal.Open>
-                                                <Modal.Window name="delete">
-                                                        <ConfirmDelete
-                                                                resourceName="cabins"
-                                                                disabled={isDeleting}
-                                                                onConfirm={() => deleteCabin(cabinId)}
-                                                        />
-                                                </Modal.Window>
+                                                                <Modal.Open opens="delete">
+                                                                        <Menus.Button icon={<HiTrash />}>
+                                                                                Delete
+                                                                        </Menus.Button>
+                                                                </Modal.Open>
+                                                        </Menus.List>
+
+                                                        <Modal.Window name="edit">
+                                                                <CreateCabinForm cabinToEdit={cabin} />
+                                                        </Modal.Window>
+
+                                                        <Modal.Window name="delete">
+                                                                <ConfirmDelete
+                                                                        resourceName="cabins"
+                                                                        disabled={isDeleting}
+                                                                        onConfirm={() => deleteCabin(cabinId)}
+                                                                />
+                                                        </Modal.Window>
+                                                </Menus.Menu>
                                         </Modal>
                                 </div>
                         </Table.Row>
--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -3,6 +3,7 @@ import Spinner from "../../ui/Spinner";
 import Table from "../../ui/Table";
 import CabinRow from "./CabinRow";
 import { useFetchCabins } from "./useFetchCabins";
+import Menus from "../../ui/Menus";
 
 // const Table = styled.div`
 //         border: 1px solid var(--color-grey-200);
@@ -33,21 +34,23 @@ function CabinTable() {
         if (isLoading) return <Spinner />;
 
         return (
-                <Table role="table" columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
-                        <Table.Header role="row">
-                                <div></div>
-                                <div>Cabin</div>
-                                <div>Capacity</div>
-                                <div>Price</div>
-                                <div>Discount</div>
-                                <div></div>
-                        </Table.Header>
+                <Menus>
+                        <Table role="table" columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+                                <Table.Header role="row">
+                                        <div></div>
+                                        <div>Cabin</div>
+                                        <div>Capacity</div>
+                                        <div>Price</div>
+                                        <div>Discount</div>
+                                        <div></div>
+                                </Table.Header>
 
-                        <Table.Body
-                                data={cabins}
-                                render={(cabin) => <CabinRow cabin={cabin} key={cabin.id}></CabinRow>}
-                        />
-                </Table>
+                                <Table.Body
+                                        data={cabins}
+                                        render={(cabin) => <CabinRow cabin={cabin} key={cabin.id}></CabinRow>}
+                                />
+                        </Table>
+                </Menus>
         );
 }
 
