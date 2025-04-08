34b946755ada3cfc7b32325bd9570636ac0b9f70
Phat117 <ntphat110705@gmail.com>
2025-04-08 19:53:31 +0700

    Building the Single Booking Page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 import Account from "./pages/Account";
+import Booking from "./pages/Booking";
 import Bookings from "./pages/Bookings";
 import Cabins from "./pages/Cabins";
 import Dashboard from "./pages/Dashboard";
@@ -34,6 +35,7 @@ function App() {
                                                 <Route index element={<Navigate replace to="dashboard" />} />
                                                 <Route path="dashboard" element={<Dashboard />} />
                                                 <Route path="bookings" element={<Bookings />} />
+                                                <Route path="bookings/:bookingId" element={<Booking />} />
                                                 <Route path="account" element={<Account />} />
                                                 <Route path="cabins" element={<Cabins />} />
                                                 <Route path="settings" element={<Settings />} />
