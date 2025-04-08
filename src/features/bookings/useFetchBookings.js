d48709ef22aa9221e62b1fca59258ea41b5240a3
Phat117 <ntphat110705@gmail.com>
2025-04-08 15:06:37 +0700

    API-Side Sorting: Sorting Bookings

--- src/features/bookings/useFetchBookings.js
+++ src/features/bookings/useFetchBookings.js
@@ -9,14 +9,21 @@ export function useFetchBookings() {
         const filterValue = searchParams.get("status");
         const filter =
                 !filterValue || filterValue === "all" ? null : { field: "status", value: filterValue, method: "eq" }; //{ field: "status", value: filterValue };
+
+        //        SortBY
+        const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+
+        const [field, direction] = sortByRaw.split("-");
+        const sortBy = { field, direction };
+
         const {
                 isLoading,
                 data: bookings,
                 error,
         } = useQuery({
-                queryKey: ["bookings", filter],
+                queryKey: ["bookings", filter, sortBy],
                 queryFn: () => {
-                        return getBookings({ filter });
+                        return getBookings({ filter, sortBy });
                 },
         });
 
