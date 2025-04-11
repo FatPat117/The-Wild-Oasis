7ad85304a9ed41fd986d583b177801d1326a534f
Phat117 <ntphat110705@gmail.com>
2025-04-11 22:59:04 +0700

    Building the Sign Up Form

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -1,3 +1,4 @@
+import { useForm } from "react-hook-form";
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
@@ -6,33 +7,70 @@ import Input from "../../ui/Input";
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
-  return (
-    <Form>
-      <FormRow label="Full name" error={""}>
-        <Input type="text" id="fullName" />
-      </FormRow>
+        const { register, formState, handleSubmit, watch } = useForm();
+        const password = watch("password");
+        const { errors } = formState;
 
-      <FormRow label="Email address" error={""}>
-        <Input type="email" id="email" />
-      </FormRow>
+        function onSubmit(data) {
+                console.log(data);
+        }
 
-      <FormRow label="Password (min 8 characters)" error={""}>
-        <Input type="password" id="password" />
-      </FormRow>
+        return (
+                <Form onSubmit={handleSubmit(onSubmit)}>
+                        <FormRow label="Full name" errors={errors?.fullName?.message}>
+                                <Input
+                                        type="text"
+                                        id="fullName"
+                                        {...register("fullName", {
+                                                required: "This field is required",
+                                        })}
+                                />
+                        </FormRow>
 
-      <FormRow label="Repeat password" error={""}>
-        <Input type="password" id="passwordConfirm" />
-      </FormRow>
+                        <FormRow label="Email address" errors={errors?.email?.message}>
+                                <Input
+                                        type="email"
+                                        id="email"
+                                        {...register("email", {
+                                                required: "This field is required",
+                                        })}
+                                />
+                        </FormRow>
 
-      <FormRow>
-        {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
-          Cancel
-        </Button>
-        <Button>Create new user</Button>
-      </FormRow>
-    </Form>
-  );
+                        <FormRow label="Password (min 8 characters)" errors={errors?.password?.message}>
+                                <Input
+                                        type="password"
+                                        id="password"
+                                        {...register("password", {
+                                                required: "This field is required",
+                                                minLength: {
+                                                        value: 8,
+                                                        message: "Password needs a minimum of 8 characters",
+                                                },
+                                        })}
+                                />
+                        </FormRow>
+
+                        <FormRow label="Repeat password" errors={errors?.passwordConfirm?.message}>
+                                <Input
+                                        type="password"
+                                        id="passwordConfirm"
+                                        {...register("passwordConfirm", {
+                                                required: "This field is required",
+                                                validate: (value) => value === password || "Passwords need to match",
+                                        })}
+                                />
+                        </FormRow>
+
+                        <FormRow>
+                                {/* type is an HTML attribute! */}
+                                <Button variation="secondary" type="reset">
+                                        Cancel
+                                </Button>
+                                <Button>Create new user</Button>
+                        </FormRow>
+                </Form>
+        );
 }
 
 export default SignupForm;
